083fb84b394fed7ca007f5281b2bc40003ca614b
alfred <alfredss.24.7@gmail.com>
2016-11-11 15:56:58 -0500

    Creamos los elementos en los Fragments

--- app/src/main/java/mundo/hola/com/example/alfre/androidpractice/FragmentA.java
+++ app/src/main/java/mundo/hola/com/example/alfre/androidpractice/FragmentA.java
@@ -5,16 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by alfre on 10/11/2016.
  */
 public class FragmentA extends Fragment{
     View rootView;
+    EditText editText;
+    Button button;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         rootView = inflater.inflate(R.layout.fragmenta, container, false);
-        return rootView;
-    }
 
+        editText = (EditText) rootView.findViewById(R.id.inputFragment);
+        button = (Button) rootView.findViewById(R.id.btnEnviaDato);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dato = editText.getText().toString();
+            }
+        });
+
+        return rootView;
+
+    }
 
 }
