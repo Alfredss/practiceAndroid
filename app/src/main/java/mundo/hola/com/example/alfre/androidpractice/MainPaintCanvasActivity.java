6acaeac8ad01fe6d65ccf32b1b060bc91de9121d
alfred <alfredss.24.7@gmail.com>
2016-11-14 15:13:13 -0500

    Trabajamos con Canvas

--- app/src/main/java/mundo/hola/com/example/alfre/androidpractice/MainPaintCanvasActivity.java
+++ app/src/main/java/mundo/hola/com/example/alfre/androidpractice/MainPaintCanvasActivity.java
@@ -1,6 +1,7 @@
 package mundo.hola.com.example.alfre.androidpractice;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -17,30 +18,35 @@ public class MainPaintCanvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Vista vista = new Vista(this);
         setContentView(vista);
-        //setContentView(R.layout.activity_main_paint_canvas);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     class Vista extends View{
         public Vista(Context context){
             super(context);
         }
-        public void onDraw(){
+
+        //@Override
+        protected void onDraw(Canvas canvas){
+            //super.onDraw(canvas);
+            /*
             Paint paint = new Paint();
             paint.setStyle(Paint.Style.STROKE);
             paint.setStrokeWidth(5);
             paint.setColor(Color.BLUE);
+            canvas.drawRect(20,20,canvas.getWidth()-50,70,paint);
+            */
+            int x = getWidth();
+            int y = getHeight();
+            int radius;
+            radius = 100;
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            canvas.drawPaint(paint);
+            // Use Color.parseColor to define HTML colors
+            paint.setColor(Color.parseColor("#CD5C5C"));
+            canvas.drawCircle(x / 2, y / 2, radius, paint);
         }
     }
 
